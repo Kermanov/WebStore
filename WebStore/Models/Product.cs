8e73bb362d8202f757fb4debcddb2a8ea22fee8c
Maksym Dovhan <kerman901@gmail.com>
2019-12-07 00:34:19 +0200

    Add Category and Product models

---
+++ WebStore/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageSource { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Price { get; set; }
+        public bool DisplayComments { get; set; }
+        
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
