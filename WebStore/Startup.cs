7a2ddc26b2e039b35d32065af88d1e2013c22b32
Maksym Dovhan <kerman901@gmail.com>
2019-12-07 00:34:48 +0200

    Create dbContext

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebStore.Context;
 
 namespace WebStore
 {
@@ -24,6 +26,11 @@ namespace WebStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<WebStoreContext>(options =>
+                options.UseSqlServer(connection));
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
