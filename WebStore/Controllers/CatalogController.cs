986a2c301b11801beaed799b9e412a5aa6620387
Maksym Dovhan <kerman901@gmail.com>
2019-12-08 02:13:51 +0200

    Add Catalog and Product controllers

---
+++ WebStore/Controllers/CatalogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+using WebStore.Repositories;
+using WebStore.Services;
+
+namespace WebStore.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly ProductService productService;
+
+        public CatalogController(ProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            var products = productService.GetAll();
+            return View(products);
+        }
+    }
+}
---
+++ WebStore/Controllers/ProductController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+using WebStore.Services;
+
+namespace WebStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductService productService;
+
+        public ProductController(ProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        // GET: Product
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            var product = productService.GetById(id);
+            return View(product);
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(string name, string description, decimal price, bool displayComments, int categoryId)
+        {
+            try
+            {
+                var newProduct = new Product
+                {
+                    Name = name,
+                    Description = description,
+                    Price = price,
+                    DisplayComments = displayComments,
+                    CategoryId = categoryId
+                };
+                productService.Create(newProduct);
+
+                return RedirectToAction(nameof(Create));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
