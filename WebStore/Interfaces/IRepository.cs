eb13a1780cae0d10644da724cfc4209f90b74bbf
Maksym Dovhan <kerman901@gmail.com>
2019-12-08 02:11:03 +0200

    Add repository interface

---
+++ WebStore/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Interfaces
+{
+    public interface IRepository<T> where T: BaseModel
+    {
+        void Create(T entity);
+        void Update(T entity);
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void DeleteById(int id);
+        IEnumerable<T> Find(Func<T, bool> predicate);
+    }
+}
