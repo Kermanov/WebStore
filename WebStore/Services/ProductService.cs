c745f55aa6bbae03ee0db22f762e2bcc18402723
Maksym Dovhan <kerman901@gmail.com>
2019-12-08 02:13:19 +0200

    Add product service

---
+++ WebStore/Services/ProductService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+using WebStore.Repositories;
+using WebStore.UnitOfWork;
+
+namespace WebStore.Services
+{
+    public class ProductService
+    {
+        private readonly WebStoreUnitOfWork unitOfWork;
+
+        public ProductService(WebStoreUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return unitOfWork.Products.GetAll();
+        }
+
+        public Product GetById(int id)
+        {
+            return unitOfWork.Products.GetById(id);
+        }
+
+        public void Create(Product product)
+        {
+            unitOfWork.Products.Create(product);
+            unitOfWork.Save();
+        }
+    }
+}
