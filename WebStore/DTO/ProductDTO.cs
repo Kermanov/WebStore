4d226d1aa1f8412c971823efb9f80af4b6c1dcff
Maksym Dovhan <kerman901@gmail.com>
2019-12-15 01:09:57 +0200

    Add product DTO class

---
+++ WebStore/DTO/ProductDTO.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.DTO
+{
+    public class ProductDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IFormFile Image { get; set; }
+        public decimal Price { get; set; }
+        public bool DisplayComments { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
