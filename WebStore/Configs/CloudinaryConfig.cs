b2120434e9a9cb6a0f01e6875a28cb345f21d210
Maksym Dovhan <kerman901@gmail.com>
2019-12-15 17:22:53 +0200

    Add CloudinaryConfig class

---
+++ WebStore/Configs/CloudinaryConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Configs
+{
+    public class CloudinaryConfig
+    {
+        public string CloudName { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+}
